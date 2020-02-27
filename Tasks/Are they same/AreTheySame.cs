7c68f2b5d6e2bff949e437104e97885f934199cc
niceman <ilya.falko2013@gmail.com>
2020-02-27 20:22:35 +0300

    Поменял структуру проекта, добавил еще одну задачу - https://www.codewars.com/kata/550498447451fbbd7600041c

---
+++ Tasks/Are they same/AreTheySame.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MrFalko
+ * Date: 27.02.2020
+ * Time: 20:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+
+namespace Tasks.Are_they_same
+{
+	
+	public class AreTheySame
+	{
+		  public static bool comp(int[] a, int[] b)
+		  {
+		  
+		    if (a == null || b == null)         return false;    
+		    if (a.Length == 0 && b.Length == 0) return true;
+		    if (a.Length != b.Length)           return false;
+		    
+		    for (int i = 0; i < a.Length; i++)  
+		          a[i] = (int)Math.Pow(a[i],2);
+		        
+		    Array.Sort<int>(a);
+		    Array.Sort<int>(b);
+		    for (int i = 0; i < a.Length; i++)  
+		        if (a[i] != b[i])       return false;
+		          
+		    return true;
+		  }
+
+	}
+}
---
+++ Tasks/Are they same/AreTheySameTests.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MrFalko
+ * Date: 27.02.2020
+ * Time: 20:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace Tasks.Are_they_same
+{
+	[TestFixture]
+	public class AreTheySameTests
+	{
+		[Test]
+	  public void TestNormal() 
+	  {
+	    int[] a = new int[] {121, 144, 19, 161, 19, 144, 19, 11};
+	    int[] b = new int[] {11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19};
+	    bool r = AreTheySame.comp(a, b); // True
+	    Assert.AreEqual(true, r);
+	  }
+	  [Test]
+	  public void TestArraysNull() 
+	  {
+	    int[] a = null;
+	    int[] b = null;
+	    bool r = AreTheySame.comp(a, b); // True
+	    Assert.AreEqual(false, r);
+	  }
+	  [Test]
+	  public void TestArraysEmpty() 
+	  {
+	    int[] a = new int[] {};
+	    int[] b = new int[] {};
+	    bool r = AreTheySame.comp(a, b); // True
+	    Assert.AreEqual(true, r);
+	  }
+	  [Test]
+	  public void TestBad() 
+	  {
+	    int[] a = new int[] {132, 144, 19, 161, 19, 144, 19, 11};
+	    int[] b = new int[] {11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19};
+	    bool r = AreTheySame.comp(a, b); // True
+	    Assert.AreEqual(false, r);
+	  }
+
+	}
+}
