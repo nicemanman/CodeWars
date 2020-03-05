f2461ea154e9d98b32cd5c96b6e067794ab9961a
niceman <ilya.falko2013@gmail.com>
2020-03-05 22:21:58 +0300

    Небольшая легкая задача - https://www.codewars.com/kata/526571aae218b8ee490006f4/train/csharp

---
+++ Tasks/Bit Counting/Test1.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MrFalko
+ * Date: 05.03.2020
+ * Time: 22:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace Tasks.Bit_Counting
+{
+	[TestFixture]
+	public class Test1
+	{
+		[Test]
+		public void CountBits()
+		{
+			Assert.AreEqual(0, Kata.CountBits(0));
+		    Assert.AreEqual(1, Kata.CountBits(4));
+		    Assert.AreEqual(3, Kata.CountBits(7));
+		    Assert.AreEqual(2, Kata.CountBits(9));
+		    Assert.AreEqual(2, Kata.CountBits(10));
+		}
+	}
+}
