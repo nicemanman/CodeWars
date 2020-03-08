324335c498adf9fa2c04707932dc3391b0e79d0c
niceman <ilya.falko2013@gmail.com>
2020-03-08 11:36:10 +0300

    Пятая задача - https://leetcode.com/problems/zigzag-conversion/

---
+++ Tasks/ZigZag Conversion/Solution1.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MrFalko
+ * Date: 08.03.2020
+ * Time: 11:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Text;
+namespace Tasks.ZigZag_Conversion
+{
+	/// <summary>
+	/// Description of Solution1.
+	/// </summary>
+	public class Solution1
+	{
+		public string Convert(string s, int numRows) {
+		    if (numRows == 1) return s;
+		    char[] symbols = s.ToCharArray();
+		    int length = symbols.Length;
+		    string[] strings = new string[numRows];
+		    int currentCol   = 0;
+		    int currentRow   = 0;
+		    bool moveUp = false;
+		    for (int i = 0; i < length; i++){  
+		        strings[currentRow] += symbols[i];
+		        currentRow = (moveUp) ? --currentRow : ++currentRow;
+		        if (currentRow == 0 || currentRow == numRows - 1) 
+		        {
+		        	moveUp = !moveUp; 
+		        	currentCol++;
+		        }
+		    }     
+		     return string.Join("", strings);
+	    }
+	}
+}
