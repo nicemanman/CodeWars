07294fe3d2404dbc4a2d6d846598fe42c1c931b6
niceman <ilya.falko2013@gmail.com>
2020-03-15 11:08:55 +0300

    Задача 6 - https://leetcode.com/problems/merge-two-sorted-lists/

--- Tasks/Add two numbers/Solution.cs
+++ Tasks/Add two numbers/Solution.cs
@@ -4,6 +4,7 @@
  * Date: 07.03.2020
  * Time: 15:39
  * https://leetcode.com/problems/add-two-numbers/
+ * https://leetcode.com/problems/merge-two-sorted-lists/
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
@@ -15,17 +16,21 @@ namespace Add_two_numbers{
       
       public ListNode(params int[] array){
       	ListNode head = this; //do not lose head!
-      	foreach (int i in array)
+      	for (int i = 0; i < array.Length; i++)
       	{
-      		head.val = i;
-      		head.next = new ListNode();
-      		head = head.next;
+      		head.val = array[i];
+      		if (i < array.Length - 1)
+      		{
+      			head.next = new ListNode();
+      			head = head.next;
+      		}
       	}
       }
       
       public void printAll(){
       	ListNode head = this; //do not lose head!
-      	while (head.next != null)
+      	
+      	while (head != null)
       	{
       		Console.Write(head.val);
       		head = head.next;
@@ -34,7 +39,6 @@ namespace Add_two_numbers{
   }
  
 public class Solution {
-    
 	public static ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode current = head;
@@ -54,13 +58,45 @@ public class Solution {
         }
         return head.next;
     }
-		
-		static void Main(){
-			ListNode l1 = new ListNode(2,4,3);
-			ListNode l2 = new ListNode(5,6,4);
+	
+	///<summary>https://leetcode.com/problems/merge-two-sorted-lists/	 
+	/// </summary>	
+	public static ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 != null) return l2;
+        if (l1 != null && l2 == null) return l1;
+        else if (l1 == null && l2 == null) return null;
+        
+        ListNode l3 = new ListNode();
+        ListNode head = l3;     
+        do 
+        {
+        	
+        	if (l1 != null && (l2 == null || l1.val <= l2.val))
+        	{	
+	        	head.next = new ListNode(l1.val);
+			    head = head.next;
+			    l1 = l1.next;
+        	}
+        	else if (l1 == null || l1.val > l2.val)
+        	{	
+	        	head.next = new ListNode(l2.val);
+			    head = head.next;
+			    l2 = l2.next;
+        	}	
+        	
 
+        } while (l1 != null || l2 != null);
+        
+        return l3.next;    
+    }
+	
+		static void Main(){
+			ListNode l1 = new ListNode(-1,0);
+			ListNode l2 = new ListNode(-2);
+		
 			ListNode l3 = AddTwoNumbers(l1, l2);
-			l3.printAll();
+			ListNode l4 = MergeTwoLists(l1, l2);
+			l4.printAll();
 			
 			Console.ReadLine();
 		}
---
+++ Tasks/Add two numbers/Test1.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MrFalko
+ * Date: 14.03.2020
+ * Time: 22:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace Add_two_numbers
+{	//TODO - Сделать возможность сравнения для элементов списка ListNode
+	//Эти тесты пока не работают по техническим причинам
+	[TestFixture]
+	public class Test1
+	{
+		[Test]
+		public void Test0()
+		{
+			ListNode l1 = new ListNode(2,2);
+			ListNode l2 = new ListNode(0,1);
+			
+			//ListNode l3 = AddTwoNumbers(l1, l2);
+			ListNode l4 = Solution.MergeTwoLists(l1, l2);
+			ListNode ok_result = new ListNode(0,1,2,2);
+			Assert.AreEqual(ok_result, l4);
+		}
+		
+		[Test]
+		public void Test2()
+		{
+			ListNode l1 = new ListNode(0,1);
+			ListNode l2 = new ListNode(2,2);
+			
+			//ListNode l3 = AddTwoNumbers(l1, l2);
+			ListNode l4 = Solution.MergeTwoLists(l1, l2);
+			Assert.AreEqual(new ListNode(0,1,2,2), l4);
+		}
+		
+		[Test]
+		public void Test3()
+		{
+			ListNode l1 = new ListNode(-2,-1);
+			ListNode l2 = new ListNode(0,1);
+			ListNode l4 = Solution.MergeTwoLists(l1, l2);
+			Assert.AreEqual(new ListNode(-2,-1,0,1), l4);
+		}
+		
+		[Test]
+		public void Test4()
+		{
+			ListNode l1 = new ListNode(2);
+			ListNode l2 = new ListNode(1);
+			
+			ListNode l4 = Solution.MergeTwoLists(l1, l2);
+			Assert.AreEqual(new ListNode(1,2), l4);
+		}
+	}
+}
